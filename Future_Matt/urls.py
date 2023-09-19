587c7d4fff3b1f95b219f3c4bc852a80351bb785
Kerbytes <mattkerbyy@gmail.com>
2023-09-20 04:23:35 +0800

    2nd commit test

---
+++ Future_Matt/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+
+]
+
+if settings.DEBUG:
+        urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
