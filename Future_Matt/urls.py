060a17a7db93308ef5879ae5a7777923fe06dca0
Kerbytes <142144345+mattkerbyy@users.noreply.github.com>
2025-10-27 03:16:33 +0800

    feat: added favicon.ico

--- Future_Matt/urls.py
+++ Future_Matt/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
+from django.views.generic import RedirectView
+
+import django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    # Serve favicon at /favicon.ico by redirecting to the static file path
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'Portfolio/images/favicon/matt-kerby-logo.ico')),
 
 ]
 
